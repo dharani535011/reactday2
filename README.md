ce73b5b17f98327901f4857265d1418039046323
dharani535011 <dharani535011@gmail.com>
2024-05-23 12:14:02 +0530

    price card completed

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Vite
+>first i open cmd prompt and install requried package for react 
+>then as usual using jsx created the price card
+>then design the page using css
+>after that using netlify upload the app in website..
---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { FaCheck } from "react-icons/fa";
+import { FaTimes  } from "react-icons/fa";
+import './App.css'
+function Card1() {
+
+  return (
+<div id='base1'>
+<p>FREE</p>
+<h1>$0/month</h1>
+<hr />
+<div>
+  <p><FaCheck /> single User</p>
+  <p><FaCheck /> 50GB Storage</p>
+  <p><FaCheck /> Unlimited Public Projects</p>
+  <p><FaCheck /> Community Access</p>
+  <p className="worng">
+  <p><FaTimes /> Unlimited Pravite Projects</p>
+  <p><FaTimes /> Dedicated Phone Support</p>
+  <p><FaTimes /> Free Subdomain</p>
+  <p><FaTimes /> Monthly Status Reports</p>
+  </p>
+</div>
+<input type="button" value="BUTTON" />
+</div>
+  )
+}
+function Card2() {
+
+  return (
+<div id='base2'>
+<p>PLUS</p>
+<h1>$9/month</h1>
+<hr />
+<div>
+  <p><FaCheck /> single User</p>
+  <p><FaCheck /> 50GB Storage</p>
+  <p><FaCheck /> Unlimited Public Projects</p>
+  <p><FaCheck /> Community Access</p>
+  <p><FaCheck /> Unlimited Pravite Projects</p>
+  <p><FaCheck /> Dedicated Phone Support</p>
+  <p><FaCheck /> Free Subdomain</p>
+  <p className="worng">
+  <p><FaTimes /> Monthly Status Reports</p>
+  </p>
+</div>
+<input type="button" value="BUTTON" />
+</div>
+  )
+}
+function Card3() {
+
+  return (
+<div id='base3'>
+<p>PRO</p>
+<h1>$49/month</h1>
+<hr />
+<div>
+  <p><FaCheck /> single User</p>
+  <p><FaCheck /> 50GB Storage</p>
+  <p><FaCheck /> Unlimited Public Projects</p>
+  <p><FaCheck /> Community Access</p>
+  <p><FaCheck /> Unlimited Pravite Projects</p>
+  <p><FaCheck /> Dedicated Phone Support</p>
+  <p><FaCheck /> Free Subdomain</p>
+  <p><FaCheck /> Monthly Status Reports</p>
+</div>
+<input type="button" value="BUTTON" />
+</div>
+  )
+}
+
+function App() {
+
+  return (
+    
+<div id='base'>
+<Card1/>
+<Card2/>
+<Card3/>
+</div>
+
+  )
+}
+
+export default App
